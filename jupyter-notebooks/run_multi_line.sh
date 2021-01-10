0eeedf78ccc22368e95da1648d29418c2ad3f6fd
Lane Rettig <lanerettig@gmail.com>
2021-01-10 11:30:23 -0500

    Bugfix

--- jupyter-notebooks/run_multi_line.sh
+++ jupyter-notebooks/run_multi_line.sh
@@ -1,1 +1,1 @@
-jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=600 crypto-multi-line-toolip-altair.ipynb
+jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=600 crypto_multi_line_toolip_altair.ipynb
